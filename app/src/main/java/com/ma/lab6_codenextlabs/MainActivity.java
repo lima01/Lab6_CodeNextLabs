0992183091e2783ec5767b9863b18e1f66047c96
golima <lima01@gmail.com>
2020-11-11 15:21:52 -0500

    Add AlertDialogs and Toasts

--- app/src/main/java/com/ma/lab6_codenextlabs/MainActivity.java
+++ app/src/main/java/com/ma/lab6_codenextlabs/MainActivity.java
@@ -2,9 +2,13 @@ package com.ma.lab6_codenextlabs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,5 +21,26 @@ public class MainActivity extends AppCompatActivity {
     public void counter(View view){
         Intent intent = new Intent(this, CounterActivity.class);
         startActivity(intent);
+    }
+    public void tbd(View view){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setMessage("More cool apps are coming!");
+        alertDialogBuilder.setTitle("Code Next Lab");
+        //alertDialogBuilder.setIcon(R.drawable.bulb);
+        //alertDialogBuilder.setIcon(android.R.drawable.ic_delete);
+        alertDialogBuilder.setIcon(android.R.drawable.ic_popup_disk_full);
+        alertDialogBuilder.setPositiveButton("Cool", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Context context = getApplicationContext();
+                CharSequence text = "Glad you are excited too!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
+
+        alertDialogBuilder.create().show();
     }
 }
